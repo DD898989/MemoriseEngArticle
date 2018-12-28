a928605dd344534476344968b900afd17b680d48
DD898989 <killuplus300@gmail.com>
2018-12-28 22:10:03 +0800

    Update MemoriseEngArticle.cs

--- MemoriseEngArticle.cs
+++ MemoriseEngArticle.cs
@@ -1,15 +1,9 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
 using System.IO;
-using System.Diagnostics;
-using System.Globalization;
 using System.Runtime.InteropServices;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
